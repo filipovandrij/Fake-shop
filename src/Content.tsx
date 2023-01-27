e9267356400d2255492252f8f07ee78ee8b97fc8
Filipov <andrijfilipov73@gmail.com>
2023-01-27 15:39:24 +0200

    add css and import him

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,16 +1,10 @@
+import './Content.css'
+
 type Props = { text1: string; text2: string; year: number }
 export const Content = (props: Props) => {
     return (
         <>
-            <p
-                style={{
-                    color: 'white',
-                    backgroundColor: 'green',
-                    padding: '15px',
-                }}
-            >
-                {props.text1}
-            </p>
+            <p className="content">{props.text1}</p>
             <p>{props.text2}</p>
             <p>Year: {props.year}</p>
         </>
