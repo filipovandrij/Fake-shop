007902473da8127a85d82f1b372ce3313804d6d4
Filipov <andrijfilipov73@gmail.com>
2023-01-31 16:32:38 +0200

    add a modules of classes

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,10 +1,13 @@
-import './Content.css'
+import classes from './Content.module.css'
 
 type Props = { text1: string; text2: string; year: number }
+
+console.log(classes)
+
 export const Content = (props: Props) => {
     return (
         <>
-            <p className="content">{props.text1}</p>
+            <p className={`${classes.text}`}>{props.text1}</p>
             <p>{props.text2}</p>
             <p>Year: {props.year}</p>
         </>
