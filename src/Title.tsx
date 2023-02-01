d8646d4a7a1c28252f865efb5e46b440daa3a57d
Filipov <andrijfilipov73@gmail.com>
2023-02-01 15:29:55 +0200

    remove files and add absolute imports

--- src/Title.tsx
+++
@@ -1,9 +0,0 @@
-interface ITitleProps {
-    text: string
-}
-
-const Title = (props: ITitleProps) => {
-    return <h1>Hello {props.text}</h1>
-}
-
-export default Title
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Content } from './Content'
-import Title from './Title'
-
-function App() {
-    return (
-        <>
-            <Title text="React" />
-            <Title text="Ts" />
-            <Content text1="Hello world 1" text2="Hello world 2" year={2023} />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
