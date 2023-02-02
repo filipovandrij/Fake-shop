f63ee09260e3005792c2a6b3ef68728db26b8bfb
Filipov <andrijfilipov73@gmail.com>
2023-02-02 15:31:20 +0200

    start nwe lesson

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -2,32 +2,6 @@ import './Style.scss'
 
 type Props = {}
 const Header = (props: Props) => {
-    return (
-        <header className="container">
-            <div>
-                <div>
-                    <h1 className="logotype">
-                        LOGO <span> this </span>
-                    </h1>
-                </div>
-                <div>
-                    <ul>
-                        <li>
-                            <a href="#">Contact</a>
-                        </li>
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">Blog</a>
-                        </li>
-                        <li>
-                            <a href="#">Product</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </header>
-    )
+    return <header className="container">Header</header>
 }
 export default Header
