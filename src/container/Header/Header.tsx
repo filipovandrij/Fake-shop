ee0c1411b0c833f6ccff0d1a13ff586960579595
Filipov <andrijfilipov73@gmail.com>
2023-02-07 12:13:56 +0200

    import logotype and add new file logo

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,10 +1,9 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
-import logo from 'assets/logo.svg'
+import Logo from 'components/Logo/Logo'
 import { Container } from '@mui/system'
 import './Header.scss'
 
@@ -23,13 +22,7 @@ const Header = (props: Props) => {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        <img src={logo} alt="logotype" />
-                    </Typography>
+                    <Logo />
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </Container>
