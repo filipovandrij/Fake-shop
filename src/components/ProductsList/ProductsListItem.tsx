842c913662c4cb4b7e4bea4456534d1513a30f4e
Filipov <andrijfilipov73@gmail.com>
2023-02-07 15:41:23 +0200

    add prods and change stuctures of props

--- src/components/ProductsList/ProductsListItem.tsx
+++ src/components/ProductsList/ProductsListItem.tsx
@@ -8,17 +8,21 @@ type Props = {
     price: number
 }
 
-const ProductsListItem = (props: Props) => {
+const ProductsListItem = ({
+    title,
+    descriptoin,
+    type,
+    capacity,
+    price,
+}: Props) => {
     return (
         <Card variant="outlined" className="product">
             <CardContent>
-                <div className="product-title">{props.title}</div>
-                <div className="product-desc">This {props.descriptoin}</div>
-                <div className="product-features">Type: {props.type}</div>
-                <div className="product-features">
-                    Capacite: {props.capacity}
-                </div>
-                <div className="product-price">{props.price}$</div>
+                <div className="product-title">{title}</div>
+                <div className="product-desc">This {descriptoin}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacite: {capacity}</div>
+                <div className="product-price">{price}$</div>
             </CardContent>
             <CardActions className="product-btn-wrap">
                 <Button variant="outlined">Add to cart</Button>
