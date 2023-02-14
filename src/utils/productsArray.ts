ba69453b4c78714afeea6e5b03caa96421799ea8
Filipov <andrijfilipov73@gmail.com>
2023-02-14 16:56:28 +0200

    add id

--- src/utils/productsArray.ts
+++ src/utils/productsArray.ts
@@ -1,4 +1,5 @@
 type Product = {
+    id: number
     title: string
     description: string
     type: string
@@ -8,6 +9,7 @@ type Product = {
 
 const productsArray:Product[] = [
     {
+        id:1,
 title: 'iPhone 14 Pro',
 description: 'This is iPhone 14 Pro',
 type: 'phone',
@@ -15,6 +17,7 @@ capacity:'2516',
 price: 2000,
     },
     {
+        id:2,
 title: 'iPhone 13 Pro',
 description: 'This is iPhone 14 Pro',
 type: 'phone',
@@ -22,6 +25,7 @@ capacity:'156',
 price: 1500,
     },
     {
+        id:3,
 title: 'iPhone 12 Pro',
 description: 'This is iPhone 12 Pro',
 type: 'phone',
@@ -29,6 +33,7 @@ capacity:'2546',
 price: 200,
     },
     {
+        id:4,
 title: 'iPhone 11 Pro',
 description: 'This is iPhone 11 Pro',
 type: 'phone',
@@ -36,6 +41,7 @@ capacity:'56',
 price: 3000,
     },
     {
+        id:5,
 title: 'iPhone 10 Pro',
 description: 'This is iPhone 10 Pro',
 type: 'phone',
@@ -43,6 +49,7 @@ capacity:'556',
 price: 12000,
     },
     {
+        id:6,
 title: 'iPhone 9 Pro',
 description: 'This is iPhone 9 Pro',
 type: 'phone',
