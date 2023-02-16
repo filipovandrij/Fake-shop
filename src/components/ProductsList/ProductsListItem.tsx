6453025b6814bd8dc87e392f1d99db53eade7f3d
Filipov <andrijfilipov73@gmail.com>
2023-02-16 15:07:42 +0200

    add btns to count

--- src/components/ProductsList/ProductsListItem.tsx
+++ src/components/ProductsList/ProductsListItem.tsx
@@ -21,11 +21,20 @@ type State = {
 }
 
 class ProductsListItem extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-            count: 2,
-        }
+    state = {
+        count: 1,
+    }
+
+    onIncrementClick = () => {
+        this.setState((prevState: State) => ({
+            count: prevState.count + 1,
+        }))
+    }
+
+    onDecrementClick = () => {
+        this.setState((prevState: State) => ({
+            count: prevState.count - 1,
+        }))
     }
 
     render() {
@@ -39,12 +48,24 @@ class ProductsListItem extends Component<Props, State> {
                     <div className="product-title">{title}</div>
                     <div className="product-desc">This {description}</div>
                     <div className="product-features">Type: {type}</div>
-                    <div className="product-features">Capacite: {capacity}</div>
+                    <div className="product-features">
+                        Capacite: {capacity} Gb
+                    </div>
                     <div className="product-price">{price}$</div>
                     <div className="product-quantity">
-                        <Button variant="outlined">-</Button>
+                        <Button
+                            variant="outlined"
+                            onClick={this.onDecrementClick}
+                        >
+                            -
+                        </Button>
                         <TextField size="small" value={this.state.count} />
-                        <Button variant="outlined">+</Button>
+                        <Button
+                            variant="outlined"
+                            onClick={this.onIncrementClick}
+                        >
+                            +
+                        </Button>
                     </div>
                 </CardContent>
                 <CardActions className="product-btn-wrap">
