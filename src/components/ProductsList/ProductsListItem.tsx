6536033d690b6cb0a43124282969688b98131a46
Filipov <andrijfilipov73@gmail.com>
2023-02-16 15:51:24 +0200

    add btn change again

--- src/components/ProductsList/ProductsListItem.tsx
+++ src/components/ProductsList/ProductsListItem.tsx
@@ -40,17 +40,9 @@ class ProductsListItem extends Component<Props, State> {
     }
 
     changeColor = () => {
-        this.setState((prevState: State) => {
-            if (prevState.color === 'green') {
-                return {
-                    color: 'red',
-                }
-            } else {
-                return {
-                    color: 'green',
-                }
-            }
-        })
+        this.setState((prevState: State) => ({
+            color: prevState.color === 'green' ? 'red' : 'green',
+        }))
     }
 
     render() {
