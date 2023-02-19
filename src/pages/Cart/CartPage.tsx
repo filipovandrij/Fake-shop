04c6e4959456305a5fde89b2da473e9033e04ba9
Filipov <andrijfilipov73@gmail.com>
2023-02-19 13:23:11 +0200

    hometask done , how? , i don`t inderestand)

--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -1,5 +1,59 @@
-type Props = {}
-const CartPage = (props: Props) => {
-    return <div>CartPage</div>
+import { Card, CardContent, Grid } from '@mui/material'
+import productsArray, { getProductsObject, Product } from 'utils/productsArray'
+
+type Props = {
+    productsInCart: {
+        [id: number]: number
+    }
+    productsObject?: {
+        [id: number]: Product
+    }
+}
+const CartPage = ({
+    productsInCart,
+    productsObject = getProductsObject(productsArray),
+}: Props) => {
+    return (
+        <Grid item xs={12} sm={6} md={4}>
+            {' '}
+            <Card variant="outlined" className="product">
+                <CardContent>
+                    {Object.keys(productsInCart).map((productId) => (
+                        <div key={productId}>
+                            <div className="product-image">
+                                <img
+                                    src={
+                                        productsObject[parseInt(productId)]
+                                            .image
+                                    }
+                                    alt=""
+                                />
+                            </div>
+                            <div className="product-title">
+                                {productsObject[parseInt(productId)].title}
+                            </div>
+                            <div className="product-desc">
+                                This{' '}
+                                {
+                                    productsObject[parseInt(productId)]
+                                        .description
+                                }
+                            </div>
+                            <div className="product-features">
+                                Type: {productsObject[parseInt(productId)].type}
+                            </div>
+                            <div className="product-features">
+                                Capacite:{' '}
+                                {productsObject[parseInt(productId)].capacity}
+                            </div>
+                            <div className="product-price">
+                                {productsObject[parseInt(productId)].price}$
+                            </div>
+                        </div>
+                    ))}
+                </CardContent>
+            </Card>{' '}
+        </Grid>
+    )
 }
 export default CartPage
